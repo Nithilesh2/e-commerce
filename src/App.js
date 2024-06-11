39a3e9cb61aef0a9c3e2bdfc969c088bc83ccd71
nithilesh <sunnybnithilesh@gmail.com>
2024-06-11 17:35:04 +0530

    errorPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/pages/Home";
-import SignUp from "./components/pages/SignUp";
-import Login from "./components/pages/Login";
-import Cart from "./components/pages/Cart";
-import CheckOut from "./components/pages/CheckOut";
-import MyAccount from "./components/pages/ManageMyAccount";
-import WishList from "./components/pages/WishList";
-import About from "./components/pages/About";
-import Contact from "./components/pages/Contact";
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./components/pages/Home"
+import SignUp from "./components/pages/SignUp"
+import Login from "./components/pages/Login"
+import Cart from "./components/pages/Cart"
+import CheckOut from "./components/pages/CheckOut"
+import MyAccount from "./components/pages/ManageMyAccount"
+import WishList from "./components/pages/WishList"
+import About from "./components/pages/About"
+import Contact from "./components/pages/Contact"
+import ErrorPage from "./components/pages/ErrorPage"
 
 const App = () => {
   return (
@@ -24,10 +25,11 @@ const App = () => {
           <Route path="/wishlist" element={<WishList />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ src/components/pages/ErrorPage.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useRef, useState } from "react"
+import style from "../css/ErrorPage.module.css"
+import { useNavigate } from "react-router-dom"
+import LoadingBar from "react-top-loading-bar"
+
+const ErrorPage = () => {
+  const navigate = useNavigate()
+  const [skeletonLoading, setSkeletonLoading] = useState(true)
+
+  useEffect(() => {
+    const randomTimeValue = Math.floor(Math.random() * 2) + 1
+    const randomTime = randomTimeValue + "000"
+
+    const timer = setTimeout(() => {
+      setSkeletonLoading(false)
+    }, randomTime)
+    return () => clearTimeout(timer)
+  }, [])
+
+  const ref = useRef(null)
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.continuousStart()
+    }
+  }, [])
+  return (
+    <>
+      {skeletonLoading ? (
+        <div className={style.skele}>
+          <div>
+            <LoadingBar color="#00BFFF" ref={ref} shadow={true} />
+          </div>
+        </div>
+      ) : (
+        <div className={style.errorPageMain}>
+          <div className={style.topLeft}>
+            <span
+              className={style.home}
+              onClick={() => {
+                navigate("/")
+              }}
+            >
+              Home
+            </span>
+            <span className={style.slash}>/</span>
+            <span className={style.myAccount}>404 Error</span>
+          </div>
+          <div className={style.bottom}>
+            <div className={style.bototmTop}>404 Page not Found</div>
+            <div className={style.bototmCenter}>
+              Hmmm, you visited unknown page
+            </div>
+            <div className={style.bototmBottom}>
+              <button
+                type="button"
+                className={style.bototmBottomButton}
+                onClick={() => {
+                  navigate("/")
+                }}
+              >
+                Back to home page
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default ErrorPage
