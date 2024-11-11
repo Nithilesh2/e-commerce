ab17a4e535a46c9472dcfd2a7245e23936853e89
nithilesh <sunnybnithilesh@gmail.com>
2024-11-11 23:52:14 +0530

    added dvh

---
+++ src/components/pages/Admin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "../css/Admin.module.css"
+
+const Admin = () => {
+  return (
+    <>
+      <div className={styles.main}>
+        
+      </div>
+    </>
+  )
+}
+
+export default Admin
