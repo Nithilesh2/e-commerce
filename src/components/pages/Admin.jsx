6af028fc5b7bc05c8ac3469ce758dcb6558a8d5d
nithilesh <sunnybnithilesh@gmail.com>
2024-11-13 23:17:52 +0530

    dashboard admin

--- src/components/pages/Admin.jsx
+++ src/components/pages/Admin.jsx
@@ -1,34 +1,91 @@
-import React from "react"
+import React, { useState } from "react"
+import { IoIosArrowDown } from "react-icons/io"
 import styles from "../css/Admin.module.css"
+import {  FaBoxOpen,FaTags, FaUsers } from 'react-icons/fa';
 
 const Admin = () => {
+  const [productsToggle, setProductsToggle] = useState(false)
+  const [sliderToggle, setSliderToggle] = useState(false)
+
   return (
     <>
       <div className={styles.main}>
         <nav className={styles.navbar}>
+          <div className={styles.leftLogo}>Exclusive</div>
+        </nav>
+        <div className={styles.adminPage}>
           <div className={styles.left}>
-            <div className={styles.leftLogo}>Exclusive</div>
             <ul className={styles.leftItems}>
               <li className={styles.leftItemsData}>Dashboard</li>
               <li className={styles.leftItemsData}>
-                Products
-                <ul className={styles.leftProducts}>
-                  <li>Insert Product</li>
-                  <li>View Products</li>
-                </ul>
+                <span onClick={() => setProductsToggle(!productsToggle)}>
+                  Products
+                  <IoIosArrowDown />
+                </span>
+                {productsToggle ? (
+                  <ul className={styles.leftProducts}>
+                    <li>Insert Product</li>
+                    <li>View Products</li>
+                  </ul>
+                ) : (
+                  ""
+                )}
               </li>
               <li className={styles.leftItemsData}>
-                Slider
-                <ul className={styles.leftSlider}>
-                  <li>Insert Slider</li>
-                  <li>View Slider</li>
-                </ul>
+                <span onClick={() => setSliderToggle(!sliderToggle)}>
+                  Slider
+                  <IoIosArrowDown />
+                </span>
+                {sliderToggle ? (
+                  <ul className={styles.leftSlider}>
+                    <li>Insert Slider</li>
+                    <li>View Slider</li>
+                  </ul>
+                ) : (
+                  ""
+                )}
               </li>
               <li className={styles.leftItemsData}>Customer Details</li>
               <li className={styles.leftItemsData}>View Payments</li>
             </ul>
           </div>
-        </nav>
+          <div className={styles.vlineAfter} />
+          <div className={styles.right}>
+            <h4 className={styles.header}>Dashboard</h4>
+            <div className={styles.rowOne}>
+              <div className={styles.orders}>
+                <div className={styles.rleft}>
+                <FaBoxOpen className={styles.rowOneIcons}/>
+                </div>
+                <div className={styles.rright}>
+                  <h4>Orders</h4>
+                  <h3>12,500</h3>
+                </div>
+              </div>
+              <div className={styles.customers}>
+                <div className={styles.rleft}>
+                  <FaUsers className={styles.rowOneIcons}/>
+                </div>
+                <div className={styles.rright}>
+                  <h4>Customers</h4>
+                  <h3>5,000</h3>
+                </div>
+              </div>
+              <div className={styles.products}>
+                <div className={styles.rleft}>
+                  <FaTags className={styles.rowOneIcons}/>
+                </div>
+                <div className={styles.rright}>
+                  <h4>Products</h4>
+                  <h3>30,000</h3>
+                </div>
+              </div>
+            </div>
+            <div className={styles.rowTwo}>
+              
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
